b65cb05512a4f08484d449d012223bb5ff17ecb5
onetoo2 <paola_qdr@hotmail.com>
2018-11-13 09:08:46 -0600

    Add files via upload

--- js/funcionesEstilos.js
+++ js/funcionesEstilos.js
@@ -5,3 +5,20 @@ function navegar(liga){
 function navegarReplace(liga){
 	location.replace(liga)
 }
+
+function calificarViaje(numEstrellas){
+	for (var i = 1; i <= numEstrellas; i++) {
+		document.getElementById('estrella'+i).style.color = '#E69E22';
+	}
+
+	var restante = 5 - numEstrellas;
+	var e;
+	for (var j = 1; j <= restante; j++) {
+		e = parseInt(numEstrellas) + j;
+		document.getElementById('estrella'+e).style.color = '#97BBC3';
+
+	}
+}
+function prueba(id){
+	alert('hola mundo');
+}
