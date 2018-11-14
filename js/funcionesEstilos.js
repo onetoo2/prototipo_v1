47fe3fd29137e3badb9260e1aa638395abe30a3c
onetoo2 <paola_qdr@hotmail.com>
2018-11-14 15:00:55 -0600

    Nuevos cambios

--- js/funcionesEstilos.js
+++ js/funcionesEstilos.js
@@ -19,6 +19,50 @@ function calificarViaje(numEstrellas){
 
 	}
 }
-function prueba(id){
-	alert('hola mundo');
+
+function mostrarMenu(){
+	menu = document.querySelector("div.menuDesplegable");
+	var bandera;
+	for (var i = 0; i <= menu.classList.length; i++) {
+
+		if(menu.classList[i] == 'esconder'){
+			bandera = 1;
+		}
+		if(menu.classList[i] == 'mostrar'){
+			bandera = 0;
+		}
+	}
+	if(bandera == 1){
+			menu.classList.remove('esconder');
+		    menu.classList.add('mostrar');
+		}
+	if(bandera == 0){
+			menu.classList.remove('mostrar');
+			menu.classList.add('esconder');
+		}
 }
+
+function mostrarHorario(id){
+	var id = 'reservar' + id;
+	horario = document.getElementById(id);
+	var bandera;
+	for (var i = 0; i <= horario.classList.length; i++) {
+
+		if(horario.classList[i] == 'esconderHorario'){
+			bandera = 1;
+		}
+		if(horario.classList[i] == 'mostrarHorario'){
+			bandera = 0;
+		}
+	}
+	if(bandera == 1){
+		horario.classList.remove('esconderHorario');
+		horario.classList.add('mostrarHorario');
+	}
+	if(bandera == 0){
+		horario.classList.remove('mostrarHorario');
+		horario.classList.add('esconderHorario');
+	}
+}
+
+
